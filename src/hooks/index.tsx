c13e719a81ed6b59319add57a7f369abb6147606
Juliano Ramos <juliano.ramos@bplus.com.br>
2022-08-03 10:12:41 -0300

    first

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,21 +1,13 @@
-import { ModalProvider, ToastProvider } from '@bplustecnologia/components-web';
 import React from 'react';
 
 import { AuthProvider } from './auth';
-import { ProgressProvider } from './progress';
 
 interface IAppProvider {
   children: React.ReactNode;
 }
 
 const AppProvider: React.FC<IAppProvider> = ({ children }) => (
-  <ProgressProvider>
-    <ModalProvider>
-      <ToastProvider>
-        <AuthProvider>{children}</AuthProvider>
-      </ToastProvider>
-    </ModalProvider>
-  </ProgressProvider>
+  <AuthProvider>{children}</AuthProvider>
 );
 
 export default AppProvider;
