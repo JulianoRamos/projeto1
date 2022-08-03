26af79df90ede498e45f3a760623ca48411b292a
Juliano Ramos <juliano.ramos@bplus.com.br>
2022-08-03 14:34:32 -0300

    first

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,7 @@ import Error401 from '../pages/Error401';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/projeto" exact component={Home} />
 
     <Route path="/error-401" component={Error401} isPrivateAndPublic />
   </Switch>
